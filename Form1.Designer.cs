1e004ec101470f0519bc378506e9bf9c634f7d65
Dmitryj <martynenko.dmitryj@gmail.com>
2016-11-04 22:13:39 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(513, 462);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Загрузить изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(503, 462);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.BackColor = System.Drawing.Color.DimGray;
+            this.splitContainer1.Location = new System.Drawing.Point(12, 76);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(1026, 462);
+            this.splitContainer1.SplitterDistance = 513;
+            this.splitContainer1.SplitterWidth = 10;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 550);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        List<Pixel> pixels;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog { Filter = "Image file|*.jpg;*.png;*.bmp" };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                var image = (Bitmap)Bitmap.FromFile(ofd.FileName);
+
+            }
+            pictureBox1.Image = new Bitmap(ofd.FileName);
+            pixels = new List<Pixel>();
+        }
+
+      
+
+        private static bool ColorMatch(Color a, Color b)
+        {
+            if (a.GetBrightness() >= 0.05f)
+                return Math.Abs(a.GetHue() - b.GetHue()) <= 45;
+            else return false;
+        }
+
+        void FloodFill(Bitmap bmp, Point pt, Color targetColor)
+        {
+            Queue<Point> q = new Queue<Point>();
+            q.Enqueue(pt);
+            while (q.Count > 0)
+            {
+                Point n = q.Dequeue();
+                if (!ColorMatch(bmp.GetPixel(n.X, n.Y), targetColor))
+                    continue;
+                Point w = n, e = new Point(n.X + 1, n.Y);
+                while ((w.X >= 0) && ColorMatch(bmp.GetPixel(w.X, w.Y), targetColor))
+                {
+                    pixels.Add(new Pixel() { x = w.X, y = w.Y, color = bmp.GetPixel(w.X, w.Y) });
+
+                    bmp.SetPixel(w.X, w.Y, Color.White);
+                    if ((w.Y > 0) && ColorMatch(bmp.GetPixel(w.X, w.Y - 1), targetColor))
+                        q.Enqueue(new Point(w.X, w.Y - 1));
+                    if ((w.Y < bmp.Height - 1) && ColorMatch(bmp.GetPixel(w.X, w.Y + 1), targetColor))
+                        q.Enqueue(new Point(w.X, w.Y + 1));
+                    w.X--;
+                }
+
+                while ((e.X <= bmp.Width - 1) && ColorMatch(bmp.GetPixel(e.X, e.Y), targetColor))
+                {
+
+                    pixels.Add(new Pixel() { x = e.X, y = e.Y, color = bmp.GetPixel(e.X, e.Y) });
+                    bmp.SetPixel(e.X, e.Y, Color.White);
+
+                    if ((e.Y > 0) && ColorMatch(bmp.GetPixel(e.X, e.Y - 1), targetColor))
+                        q.Enqueue(new Point(e.X, e.Y - 1));
+                    if ((e.Y < bmp.Height - 1) && ColorMatch(bmp.GetPixel(e.X, e.Y + 1), targetColor))
+                        q.Enqueue(new Point(e.X, e.Y + 1));
+                    e.X++;
+                }
+            }
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            pictureBox3.Image = new Bitmap(bmp.Width, bmp.Height);
+            Bitmap bmp2 = new Bitmap(pictureBox3.Image);
+            Thread t = new Thread(() =>
+            {
+                FloodFill(bmp, e.Location, bmp.GetPixel(e.X, e.Y));
+            });
+            
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            t.Start();
+            t.Join(2000);
+            t.Abort();
+            stopWatch.Stop();
+            if (stopWatch.ElapsedMilliseconds < 1000)
+            {
+                foreach (Pixel p in pixels)
+                {
+                    bmp2.SetPixel(p.x, p.y, p.color);
+                }
+                pictureBox3.Image = bmp2;
+                if (pixels.Count > 0)
+                {
+                    string[] text = new string[4];
+                    text[0] = "West: (" + pixels.OrderByDescending((x => x.x)).Last().x + ";" + pixels.OrderByDescending((x => x.x)).Last().y + ")";
+                    text[1] = "East: (" + pixels.OrderByDescending((x => x.x)).First().x + ";" + pixels.OrderByDescending((x => x.x)).First().y + ")";
+                    text[2] = "North: (" + pixels.OrderByDescending((x => x.y)).Last().x + ";" + pixels.OrderByDescending((x => x.y)).Last().y + ")";
+                    text[3] = "South: (" + pixels.OrderByDescending((x => x.y)).First().x + ";" + pixels.OrderByDescending((x => x.y)).First().y + ")";
+                    File.WriteAllLines(Application.StartupPath + @"/coord.txt", text);
+                }
+                pixels.Clear();
+            }
+        }
+    }
+    public struct Pixel
+    {
+        public int x;
+        public int y;
+        public Color color;
+    }
+}
+
+   
+
+
+        
+    
